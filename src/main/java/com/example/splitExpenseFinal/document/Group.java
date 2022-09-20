c106195f8b22cc5683369b1fd47a7d4e08d10f1b
rohan.j <rohan.j@gdn-commerce.com>
2022-09-20 14:23:32 +0530

    Initial commit

---
+++ src/main/java/com/example/splitExpenseFinal/document/Group.java
@@ -0,0 +1,50 @@
+package com.example.splitExpenseFinal.document;
+
+import com.sun.istack.internal.NotNull;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.HashMap;
+import java.util.Map;
+
+@Document
+public class Group {
+    @Id
+    private String id;
+    private String name;
+    private Map<String,Double> currentBalance = new HashMap<>();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map<String, Double> getCurrentBalance() {
+        return currentBalance;
+    }
+
+    public void setCurrentBalance(Map<String, Double> currentBalance) {
+        this.currentBalance = currentBalance;
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", currentBalance=" + currentBalance +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/splitExpenseFinal/document/User.java
@@ -0,0 +1,56 @@
+package com.example.splitExpenseFinal.document;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class User {
+    @Id
+    private String id;
+    private String name;
+    private double balance=0.0;
+    private double spent=0.0;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getSpent() {
+        return spent;
+    }
+
+    public void setSpent(double spent) {
+        this.spent = spent;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", balance=" + balance +
+                ", spent=" + spent +
+                '}';
+    }
+}
